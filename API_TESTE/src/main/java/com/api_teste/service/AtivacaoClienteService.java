c43eaca93bb72df59f63af6ae9269d5aeff90996
Vinicius de Oliveira Almeida <gvweb.solucoes@gmail.com>
2022-09-28 20:30:01 -0300

    2.11. Definindo beans com @Component

---
+++ API_TESTE/src/main/java/com/api_teste/service/AtivacaoClienteService.java
@@ -0,0 +1,18 @@
+package com.api_teste.service;
+
+import org.springframework.stereotype.Component;
+
+import com.api_teste.modelo.Cliente;
+import com.api_teste.notificacao.NotificadorEmail;
+
+@Component
+public class AtivacaoClienteService {
+	
+	private NotificadorEmail notificador;
+	
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+		
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
+	}
+}
---
+++ API_TESTE/src/test/java/com/api_teste/ApiTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
